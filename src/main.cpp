f296f4846675e64923703d1d69cc560d95e7b29f
BenjaminBrd <benjamin.bourdiau-camino@etu-upsaclay.fr>
2026-05-28 10:31:17 +0200

    6 sliders + 2 potards in case

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,98 @@
 #include "lvgl.h"
 
-static void event_handler(lv_event_t * e)
-{
-    lv_event_code_t code = lv_event_get_code(e);
 
-    if(code == LV_EVENT_CLICKED) {
-        LV_LOG_USER("Clicked");
-    }
-    else if(code == LV_EVENT_VALUE_CHANGED) {
-        LV_LOG_USER("Toggled");
-    }
-}
-
-static void slider_event_cb(lv_event_t * e)
+/*static void slider_event_cb(lv_event_t * e)
 {
-    // On récupère le slider qui a déclenché l'événement
     lv_obj_t * slider = (lv_obj_t *)lv_event_get_target(e);
     
-    // On récupère le label qu'on a passé en user_data
     lv_obj_t * label = (lv_obj_t *)lv_event_get_user_data(e);
 
-    // On lit la valeur actuelle du slider
     int32_t value = lv_slider_get_value(slider);
 
-    // On met à jour le texte du label avec la nouvelle valeur
     lv_label_set_text_fmt(label, "Valeur : %d", (int)value);
-}
+}*/
 
 void gestionScreen()
 {
-  // Initialisations générales
-  lv_obj_t * label = lv_label_create(lv_screen_active());
+  lv_obj_t * treble = lv_label_create(lv_screen_active());
+  lv_label_set_text(treble, "Treble");
+  lv_obj_set_style_text_color(treble, lv_color_hex(0xFFFFFF), LV_PART_MAIN);
+  lv_obj_align(treble, LV_ALIGN_TOP_LEFT, 17, 5);
 
-  lv_label_set_text(label, "Valeur : 0");
-  lv_obj_set_style_text_color(label, lv_color_hex(0xFFFFFF), LV_PART_MAIN);
-  lv_obj_align(label, LV_ALIGN_CENTER, 0, -100); 
+  lv_obj_t * mid = lv_label_create(lv_screen_active());
+  lv_label_set_text(mid, "Mid");
+  lv_obj_set_style_text_color(mid, lv_color_hex(0xFFFFFF), LV_PART_MAIN);
+  lv_obj_align(mid, LV_ALIGN_TOP_LEFT, 127, 5);
 
-  lv_obj_t * slider = lv_slider_create(lv_screen_active());
-  lv_slider_set_range(slider, 0, 10);
-  lv_obj_set_size(slider, 20, 150);
-  lv_obj_center(slider);
+  lv_obj_t * bass = lv_label_create(lv_screen_active());
+  lv_label_set_text(bass, "Bass");
+  lv_obj_set_style_text_color(bass, lv_color_hex(0xFFFFFF), LV_PART_MAIN);
+  lv_obj_align(bass, LV_ALIGN_TOP_MID, 0, 5);
 
-  lv_obj_add_event_cb(slider, slider_event_cb, LV_EVENT_VALUE_CHANGED, label);
+  lv_obj_t * sliderTr1 = lv_slider_create(lv_screen_active());
+  lv_slider_set_range(sliderTr1, 0, 10);
+  lv_obj_set_size(sliderTr1, 20, 100);
+  lv_obj_align(sliderTr1, LV_ALIGN_TOP_LEFT, 30, 25);
+
+  lv_obj_t * sliderMid1 = lv_slider_create(lv_screen_active());
+  lv_slider_set_range(sliderMid1, 0, 10);
+  lv_obj_set_size(sliderMid1, 20, 100);
+  lv_obj_align(sliderMid1, LV_ALIGN_TOP_LEFT, 130, 25);
+
+  lv_obj_t * sliderBass1 = lv_slider_create(lv_screen_active());
+  lv_slider_set_range(sliderBass1, 0, 10);
+  lv_obj_set_size(sliderBass1, 20, 100);
+  lv_obj_align(sliderBass1, LV_ALIGN_TOP_MID, 0, 25);
+
+  lv_obj_t * sliderTr2 = lv_slider_create(lv_screen_active());
+  lv_slider_set_range(sliderTr2, 0, 10);
+  lv_obj_set_size(sliderTr2, 20, 100);
+  lv_obj_align(sliderTr2, LV_ALIGN_BOTTOM_LEFT, 30, -15);
+
+  lv_obj_t * sliderMid2 = lv_slider_create(lv_screen_active());
+  lv_slider_set_range(sliderMid2, 0, 10);
+  lv_obj_set_size(sliderMid2, 20, 100);
+  lv_obj_align(sliderMid2, LV_ALIGN_BOTTOM_LEFT, 130, -15);
+
+  lv_obj_t * sliderBass2 = lv_slider_create(lv_screen_active());
+  lv_slider_set_range(sliderBass2, 0, 10);
+  lv_obj_set_size(sliderBass2, 20, 100);
+  lv_obj_align(sliderBass2, LV_ALIGN_BOTTOM_MID, 0, -15);
+
+  lv_obj_t * vol_panel = lv_obj_create(lv_screen_active());
+  lv_obj_set_size(vol_panel, 150, 250); 
+  lv_obj_align(vol_panel, LV_ALIGN_RIGHT_MID, -30, 0);
+  
+  lv_obj_set_style_bg_color(vol_panel, lv_color_hex(0x000000), LV_PART_MAIN); 
+  lv_obj_set_style_radius(vol_panel, 10, LV_PART_MAIN);
+  lv_obj_set_style_border_width(vol_panel, 0, LV_PART_MAIN);
+  lv_obj_set_scrollbar_mode(vol_panel, LV_SCROLLBAR_MODE_OFF); 
+
+  /*lv_obj_t * vol = lv_label_create(vol_panel);
+  lv_label_set_text(vol, "Volume");
+  lv_obj_set_style_text_color(vol, lv_color_hex(0xFFFFFF), LV_PART_MAIN);
+  lv_obj_align(vol, LV_ALIGN_TOP_MID, 0, 0); */
+
+  lv_obj_t * volume1 = lv_arc_create(vol_panel);
+  lv_arc_set_range(volume1, 0, 10);
+  lv_arc_set_bg_angles(volume1, 135, 45);
+  lv_obj_set_size(volume1, 110, 110);
+  lv_obj_remove_flag(volume1, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_align(volume1, LV_ALIGN_CENTER, 0, -55); 
+
+  lv_obj_t * volume2 = lv_arc_create(vol_panel);
+  lv_arc_set_range(volume2, 0, 10);
+  lv_arc_set_bg_angles(volume2, 135, 45);
+  lv_obj_set_size(volume2, 110, 110);
+  lv_obj_remove_flag(volume2, LV_OBJ_FLAG_SCROLLABLE);
+  lv_obj_align(volume2, LV_ALIGN_CENTER, 0, 65); 
+
+  /*lv_obj_add_event_cb(sliderTr1, slider_event_cb, LV_EVENT_VALUE_CHANGED, label);
+  lv_obj_add_event_cb(sliderMid1, slider_event_cb, LV_EVENT_VALUE_CHANGED, label);
+  lv_obj_add_event_cb(sliderBass1, slider_event_cb, LV_EVENT_VALUE_CHANGED, label);
+  lv_obj_add_event_cb(sliderTr2, slider_event_cb, LV_EVENT_VALUE_CHANGED, label);
+  lv_obj_add_event_cb(sliderMid2, slider_event_cb, LV_EVENT_VALUE_CHANGED, label);
+  lv_obj_add_event_cb(sliderBass2, slider_event_cb, LV_EVENT_VALUE_CHANGED, label);*/
   
   lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x2D2D2D), LV_PART_MAIN);
   lv_obj_set_style_bg_opa(lv_screen_active(), LV_OPA_COVER, LV_PART_MAIN);
