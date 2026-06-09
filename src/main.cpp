1db0495c573bd4daeb1ba018b45fddf7ed04a6c9
BenjaminBrd <benjamin.bourdiau-camino@etu-upsaclay.fr>
2026-06-09 17:23:12 +0200

    knob img fonctionnel + debut potards

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include <SPI.h>
 
-LV_IMAGE_DECLARE(pot);
+LV_IMAGE_DECLARE(pot100x100);
 LV_IMAGE_DECLARE(sliderKnob40x25);
 
 const int SPI_CS_PIN = 3;
@@ -20,7 +20,9 @@ lv_obj_t * sliderTr1;
 lv_obj_t * sliderMid1;
 lv_obj_t * sliderBass1;
 lv_obj_t * volume1;
+lv_obj_t * btn_casque1;
 lv_obj_t * volume2;
+lv_obj_t * btn_casque2;
 
 void mcp42010Write(uint8_t command, uint8_t value)
 {
@@ -76,6 +78,26 @@ void init_TDA7439() {
   Serial.println("tda on");
 }
 
+static void mute_event_cb(lv_event_t * e)
+{
+    lv_obj_t * target = (lv_obj_t *)lv_event_get_target(e);
+
+    if (target == btn_casque1) {
+        if(lv_obj_has_state(target, LV_STATE_CHECKED)) {
+            mcp42010Shutdown(MCP42010_POT_0); 
+        } else {
+            mcp42010SetPot(MCP42010_POT_0, lv_arc_get_value(volume1)); 
+        }
+    } 
+    else if (target == btn_casque2) {
+        if(lv_obj_has_state(target, LV_STATE_CHECKED)) {
+            mcp42010Shutdown(MCP42010_POT_1); 
+        } else {
+            mcp42010SetPot(MCP42010_POT_1, lv_arc_get_value(volume2)); 
+        }
+    }
+}
+
 void gestionScreen()
 {
   static lv_style_t style_arc_bg, style_arc_indicator, style_arc_knob;
@@ -115,10 +137,10 @@ void gestionScreen()
       lv_style_init(&style_fader_knob);
       lv_style_set_bg_opa(&style_fader_knob, 0);
       lv_style_set_bg_image_src(&style_fader_knob, &sliderKnob40x25);
-      lv_style_set_pad_left(&style_fader_knob, 16);
-      lv_style_set_pad_right(&style_fader_knob, 16);
-      lv_style_set_pad_top(&style_fader_knob, 12);
-      lv_style_set_pad_bottom(&style_fader_knob, 12);
+      lv_style_set_pad_left(&style_fader_knob, 22);
+      lv_style_set_pad_right(&style_fader_knob, 22);
+      lv_style_set_pad_top(&style_fader_knob, 18);
+      lv_style_set_pad_bottom(&style_fader_knob, 18);
 
       styles_inited = true;
   }
@@ -184,6 +206,13 @@ void gestionScreen()
   lv_obj_add_style(volume1, &style_arc_indicator, LV_PART_INDICATOR);
   lv_obj_add_style(volume1, &style_arc_knob, LV_PART_KNOB);
 
+  btn_casque1 = lv_image_create(volume1);
+  lv_image_set_src(btn_casque1, &pot100x100);
+  lv_obj_align(btn_casque1, LV_ALIGN_CENTER, 0, 0);
+  lv_obj_add_flag(btn_casque1, LV_OBJ_FLAG_CLICKABLE);
+  lv_obj_add_flag(btn_casque1, LV_OBJ_FLAG_CHECKABLE);
+  lv_obj_add_event_cb(btn_casque1, mute_event_cb, LV_EVENT_VALUE_CHANGED, NULL);
+
   volume2 = lv_arc_create(vol_panel);
   lv_arc_set_range(volume2, 0, 255);
   lv_arc_set_value(volume2, 128);
@@ -194,6 +223,13 @@ void gestionScreen()
   lv_obj_add_style(volume2, &style_arc_bg, LV_PART_MAIN);
   lv_obj_add_style(volume2, &style_arc_indicator, LV_PART_INDICATOR);
   lv_obj_add_style(volume2, &style_arc_knob, LV_PART_KNOB);
+
+  btn_casque2 = lv_image_create(volume2);
+  lv_image_set_src(btn_casque2, &pot100x100);
+  lv_obj_align(btn_casque2, LV_ALIGN_CENTER, 0, 0);
+  lv_obj_add_flag(btn_casque2, LV_OBJ_FLAG_CLICKABLE);
+  lv_obj_add_flag(btn_casque2, LV_OBJ_FLAG_CHECKABLE);
+  lv_obj_add_event_cb(btn_casque1, mute_event_cb, LV_EVENT_VALUE_CHANGED, NULL);
 
   lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x2D2D2D), LV_PART_MAIN);
   lv_obj_set_style_bg_opa(lv_screen_active(), LV_OPA_COVER, LV_PART_MAIN);
